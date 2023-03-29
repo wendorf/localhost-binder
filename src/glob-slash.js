d04c818c13deea3ace79ab87677ec622c546b18b
Dan Wendorf <dan@render.com>
2023-03-29 16:13:06 -0700

    Remove all external dependencies

--- src/glob-slash.js
+++
@@ -1,4 +0,0 @@
-import path from 'node:path';
-export const normalize = value => path.posix.normalize(path.posix.join('/', value));
-
-export default value => (value.charAt(0) === '!' ? `!${normalize(value.substr(1))}` : normalize(value));
